6650505f7c67b069bb6434433f2fda2673a64308
Vaishnav88sk <vaishnavsk8804@gmail.com>
2025-02-13 15:45:27 +0530

    Changes updated in live app

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = 'muw(_%aavu3qkbrhj04+e@1!fqt-by%2w(m0gax8p8uo-)36^g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','15.207.89.103'] 
+ALLOWED_HOSTS = ['*','minglechat.duckdns.org','minglechat.com','minglechats.com']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://minglechat.duckdns.org',
+]
 
 
 # Application definition
@@ -83,10 +87,21 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'minglechat',
+	'USER': 'admin',
+	'PASSWORD': 'Minglechat123',
+	'HOST': 'minglechat.cnaoe2uceuue.ap-south-1.rds.amazonaws.com',
+	'PORT': '3306',
     }
 }
 
--- users/views.py
+++ users/views.py
@@ -10,8 +10,8 @@ def login_page(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        print(username)
-        print(password)
+        # print(username)
+        # print(password)
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
